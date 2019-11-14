11f7c99c1766e4688b1416f3a2606ed20e561a10
sagar-vasu <55204464+sagar-vasu@users.noreply.github.com>
2019-11-14 16:44:02 +0500

    updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import'firebase/auth'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD4Qw9KZiGIsA4rsvfZT3PNOMoBHCTFXh8",
-    authDomain: "quiz-app-1f780.firebaseapp.com",
-    databaseURL: "https://quiz-app-1f780.firebaseio.com",
-    projectId: "quiz-app-1f780",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "92314454447",
-    appId: "1:92314454447:web:e070fa8a42fae413"
+    messagingSenderId: "",
+    appId: ""
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 var provider = new firebase.auth.FacebookAuthProvider();
